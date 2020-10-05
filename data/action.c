d2a25c72d5a1b7b4ac095cac9dac811cd02edcf8
unknown <mahes@LAPTOP-PS3GIE82>
2020-10-05 21:24:37 +0530

    Testing from vugen to Git

---
+++ data/action.c
@@ -0,0 +1,183 @@
+Action()
+{
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_cookie("rxVisitor=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O; DOMAIN=localhost");
+
+	web_add_cookie("dtPC=2$221998290_833h-vUHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0e5; DOMAIN=localhost");
+
+	web_add_cookie("rxvt=1601623798970|1601621998302; DOMAIN=localhost");
+
+	web_add_cookie("dtSa=-; DOMAIN=localhost");
+
+	web_add_cookie("dtLatC=63; DOMAIN=localhost");
+
+	web_custom_request("rb_bf70382eeh", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fheader.html&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$a=0%7C1%7C_load_%7CS%2C1%7C2%7C_load_%7C_load_%7C-%7C1601621998350%7C1601621998556%7C12%7C-%7C-%7C-%7C-%7C-%7Chttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2F%2C2%7C11%7C_event_%7C1601621998350%7C_vc_%7CV%7C181%5Epc%7CVCS%7C1601621998676%7CVE%7C0%5Ep19%5Ep3105%5Ept%5EsIMG%7CS%7C158%2C2%7C3%7C_onload_%7C_load_%7C-%7C1601621998516%7C1601621998554%7C12$dO=localhost,$pId=221998290_833$pFId=221998290_833$rId=RID_636585166$rpId=15698139$domR=1601621998515$w=1920$h=972$sw=1920$sh=1080$nt="
+		"a0b1601621998350e0f0g0h0i0k83l85m85o160p160q165r165s168t169$url=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fheader.html$title=$latC=63$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998489_8$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601621999760$cs=-1806315855", 
+		LAST);
+
+	web_add_cookie("dtLatC=5; DOMAIN=localhost");
+
+	web_custom_request("rb_bf70382eeh_2", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$a=0%7C1%7C_load_%7CS%2C1%7C4%7C_load_%7C_load_%7C-%7C1601621998406%7C1601621998990%7C7%7C2%7C-%7C-%7C-%7C-%7Chttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2F%2C2%7C12%7C_event_%7C1601621998406%7C_vc_%7CV%7C245%5Epc%7CVCS%7C1601621999050%7CVE%7C0%5Ep0%5Ep1733760%5Ept%5EsFRAMESET%7CS%7C245%2C2%7C9%7C_onload_%7C_load_%7C-%7C1601621998832%7C1601621998988%7C7$dO=localhost,$pId=221998290_833$pFId=221998290_833$rId=RID_731518451$rpId=-2125249050$domR=1601621998832$w=1920$h=972$sw=1920$sh=1080$nt="
+		"a0b1601621998406e0f0g0h0i0k153l153m154o247p247q248r426s428t428$url=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue$title=Web%20Tours$latC=5$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998591_647$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622000108$cs=1434031205", 
+		LAST);
+
+	web_custom_request("rb_bf70382eeh_3", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2F&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$a=1%7C1%7C_load_%7C_load_%7C-%7C1601621997902%7C1601621998993%7C7%7C2%2C2%7C13%7C_event_%7C1601621997902%7C_vc_%7CV%7C511%5Epc%7CVCS%7C1601621999050%7CVE%7C0%5Ep0%5Ep1866240%5Ept%5EsFRAMESET%7CS%7C511%2C2%7C10%7C_onload_%7C_load_%7C-%7C1601621998970%7C1601621998991%7C7$dO=localhost,$rId=RID_-1111432344$rpId=-191917823$domR=1601621998969$w=1920$h=972$sw=1920$sh=1080$nt=a0b1601621997902f2g2h2i2k2l2m2o528p528q530r1067s1070t1070$url=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2F$title="
+		"Web%20Tours$latC=5$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998290_833$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$nV=1$nVAT=1$time=1601622000126$cs=-1704664593", 
+		LAST);
+
+	web_custom_request("rb_bf70382eeh_4", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fhome.html&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/WebTours/home.html", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$a=0%7C4%7C_load_%7CS%2C1%7C5%7C_load_%7C_load_%7C-%7C1601621998646%7C1601621998789%7C40%7C-%7C-%7C-%7C-%7C-%7Chttp%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue%2C2%7C14%7C_event_%7C1601621998646%7C_vc_%7CV%7C98%5Epc%7CVCS%7C1601621998994%7CVE%7C2%5Ep2%5Ep1575048%5Ept%5EsBODY%7CS%7C98%2C2%7C6%7C_onload_%7C_load_%7C-%7C1601621998748%7C1601621998786%7C40$dO=localhost,$pId=221998290_833$pFId=221998591_647$rId=RID_89734112$rpId=1676695220$domR=1601621998747$w=1920$h=972$sw="
+		"1920$sh=1080$nt=a0b1601621998646e0f0g0h0i0k35l37m37o99p99q101r101s104t104$url=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fhome.html$title=Web%20Tours$latC=5$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998720_905$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622000163$cs=607157522", 
+		LAST);
+
+	web_add_cookie("dtLatC=11; DOMAIN=localhost");
+
+	web_custom_request("rb_bf70382eeh_5", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fnav.pl%3Fin%3Dhome&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$a=0%7C4%7C_load_%7CS%2C1%7C7%7C_load_%7C_load_%7C-%7C1601621998622%7C1601621998985%7C44%7C-%7C-%7C-%7C-%7C-%7Chttp%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue%2C2%7C15%7C_event_%7C1601621998622%7C_vc_%7CV%7C201%5Epc%7CVCS%7C1601621999047%7CVE%7C2%5Ep2%5Ep32916%5Ept%5EsBODY%7CS%7C201%2C2%7C8%7C_onload_%7C_load_%7C-%7C1601621998828%7C1601621998982%7C44$dO=localhost,$pId=221998290_833$pFId=221998591_647$rId=RID_167338034$rpId=1423062929$domR=1601621998827$w=1920$h=972$sw="
+		"1920$sh=1080$nt=a0b1601621998622k125l126m126o203p203q205r205s208t208$url=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fnav.pl%3Fin%3Dhome$title=Web%20Tours%20Navigation%20Bar$latC=11$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998805_923$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622000256$cs=-1154162565", 
+		LAST);
+
+	web_add_cookie("dtLatC=7; DOMAIN=localhost");
+
+	web_custom_request("rb_bf70382eeh_6", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fheader.html&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$3p=2-1601621998350%3Blocalhost%7C2%7C2%7C0%7C0%7C0%7C0%7C2%7C%7C0%7C0%7C0%7C105_181%7C59%7C41%7C76$rt=2-1601621998350%3Bhttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fimages%2Fhp_5Flogo.png%7Cb105e6f6g6h6i6k6l23m76E2F3105O69P45I7%7Chttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fimages%2Fwebtours.png%7Cb111e7f7g7h7i7k9l28m41E1F5850O130P45I7$url=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fheader.html$title=$latC=7$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998489_8$v="
+		"10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622001830$cs=-345041187", 
+		LAST);
+
+	web_custom_request("rb_bf70382eeh_7", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$3p=4-1601621998406%3Blocalhost%7Cg%7C2%7C0%7C0%7C0%7C217_418%7C150%7C98%7C201$rt=4-1601621998406%3Bhttp%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fnav.pl%3Fin%3Dhome%7Cb217e126f126g126h126i126k126l127m201BiI6%7Chttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fhome.html%7Cb237e35f35g35h35i35k35l37m98BiI6$url=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue$title=Web%20Tours$latC=5$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998591_647$v="
+		"10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622002180$cs=-914040165", 
+		LAST);
+
+	web_custom_request("rb_bf70382eeh_8", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2F&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$3p=1-1601621997902%3Blocalhost%7Cg%7C2%7C0%7C0%7C0%7C449_748%7C201%7C158%7C244$rt=1-1601621997902%3Bhttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fheader.html%7Cb449e83f83g83h83i83k83l85m158BiI6%7Chttp%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fwelcome.pl%3FsignOff%3Dtrue%7Cb504e153f153g153h153i153k153l153m244BiI6$url=http%3A%2F%2Flocalhost%3A1080%2FWebTours%2F$title=Web%20Tours$latC=5$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998290_833$v=10201200909073022$vID="
+		"1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time=1601622002198$cs=2067654224", 
+		LAST);
+
+	web_add_cookie("dtLatC=9; DOMAIN=localhost");
+
+	web_custom_request("rb_bf70382eeh_9", 
+		"URL=http://localhost:1080/rb_bf70382eeh?type=js3&sn=v_4_srv_2_sn_A90179D7D3344DA34E59A965CFED19CA_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1&svrid=2&flavor=post&vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0&modifiedSince=1601365711296&rf=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fnav.pl%3Fin%3Dhome&bp=3&app=ea7c4b59f27d43eb&end=1", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/plain", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		"EncType=text/plain;charset=UTF-8", 
+		"Body=$3p=7-1601621998622%3Blocalhost%7C2%7C1%7C0%7C0%7C0%7C0%7C1%7C%7C0%7C0%7C0%7C153_187%7C33%7C33%7C33$rt=7-1601621998622%3Bhttp%3A%2F%2Flocalhost%3A1080%2FWebTours%2Fimages%2Fmer_5Flogin.gif%7Cb153e9f9g9h9i9k9l32m33I22$url=http%3A%2F%2Flocalhost%3A1080%2Fcgi-bin%2Fnav.pl%3Fin%3Dhome$title=Web%20Tours%20Navigation%20Bar$latC=9$app=ea7c4b59f27d43eb$vi=UHPRFAASHJFMCIRKJSNPUITMCHACHKIP-0$vs=2$fId=221998805_923$v=10201200909073022$vID=1601621998299GN4Q75FVKR2A2OKE31JGT1NDDEMCUO0O$time="
+		"1601622002331$cs=-894679881", 
+		LAST);
+
+	lr_start_transaction("2_login");
+
+	lr_end_transaction("2_login",LR_AUTO);
+
+	lr_start_transaction("03_flights");
+
+	lr_end_transaction("03_flights",LR_AUTO);
+
+	lr_start_transaction("03_find flight_conitnue");
+
+	lr_end_transaction("03_find flight_conitnue",LR_AUTO);
+
+	lr_start_transaction("04_select flight");
+
+	lr_end_transaction("04_select flight",LR_AUTO);
+
+	lr_start_transaction("05_payment");
+
+	lr_end_transaction("05_payment",LR_AUTO);
+
+	lr_start_transaction("06_logout");
+
+	lr_end_transaction("06_logout",LR_AUTO);
+
+	return 0;
+}
